ff64854c01d29f618a01748c7eefd50d7f01a4fc
Pedro <50713178+prgf87@users.noreply.github.com>
2024-01-03 20:51:52 +0000

    completes emoji picker

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import IconButton from "./components/IconButton";
 import CircleButton from "./components/CircleButton";
 import EmojiPicker from "./components/EmojiPicker";
 import EmojiList from "./components/EmojiList";
+import EmojiSticker from "./components/EmojiSticker";
 
 const PlaceholderImage = require("./assets/images/background-image.jpg");
 
@@ -47,6 +48,9 @@ export default function App() {
           placeholderImageSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
+        {pickedEmoji !== null ? (
+          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+        ) : null}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
