3ab2e5e818d51402868b2cb152b05d04db9a5c5b
Pedro <50713178+prgf87@users.noreply.github.com>
2024-01-03 19:28:27 +0000

    cors issues

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const fetchImage = async () => {
+    const response = await fetch(
+      "https://unsplash.com/photos/a-picture-of-a-tree-with-white-lines-on-it-yJDDRS6U9OQ",
+      {
+        method: "GET",
+        mode: "no-cors",
+      }
+    );
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchImage().then((res) => {
+      console.log(res);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
   },
 });
